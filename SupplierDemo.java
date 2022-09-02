01eb9b00fc560cc82b06155324d6848d56c58a3f
Aditii0102 <87201940+Aditii0102@users.noreply.github.com>
2022-09-02 12:51:05 +0530

    Add files via upload

---
+++ SupplierDemo.java
@@ -0,0 +1,14 @@
+package javaspecification.day2;
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+
+public class SupplierDemo {
+
+	public static void main(String[] args) {
+		Supplier<LocalDateTime>supplier=
+		// TODO Auto-generated method stub
+
+	}
+
+}
